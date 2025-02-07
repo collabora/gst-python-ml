565dcc9404dbd1de42e90c3168dc0c8faf0cff8a
Aaron Boxer <aaron.boxer@collabora.com>
2025-02-07 14:51:46 -0500

    tidy

--- plugins/python/log/gst_logger.py
+++ plugins/python/log/gst_logger.py
@@ -21,8 +21,7 @@ import gi
 
 gi.require_version("Gst", "1.0")
 from gi.repository import Gst  # noqa: E402
-from .logger import Logger
-
+from .logger import Logger  # noqa: E402
 
 class GstLogger(Logger):
     """Logger implementation using GStreamer with caller file and line number."""
