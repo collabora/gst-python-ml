c136fd61bec13748a829ba27f690deb364976cf3
Aaron Boxer <aaron.boxer@collabora.com>
2025-05-02 12:32:40 -0400

    caption pipelines: switch back to autovideosink

--- pipelines/filter_dual_stream.py
+++ pipelines/filter_dual_stream.py
@@ -11,8 +11,8 @@ pipeline_desc = """
   filesrc location=data/soccer_single_camera.mp4 ! decodebin ! videoconvertscale ! video/x-raw,width=640,height=480,format=RGB ! pyml_streammux name=mux
   filesrc location=data/soccer_tracking.mp4 ! decodebin ! videoconvertscale ! video/x-raw,width=640,height=480,format=RGB ! mux.
   mux. ! pyml_llmstreamfilter name=filter device=cuda:0 downsampled_width=320 downsampled_height=240 num-streams=2 llm-model-name=microsoft/phi-2 caption-file=data/sample_captions.json prompt="Choose the {n} most interesting captions from the following list:\n{captions}" ! pyml_streamdemux name=demux
-  demux.src_0 ! queue ! videoconvert ! textoverlay name=overlay0 ! videoconvert ! glimagesink sync=false
-  demux.src_1 ! queue ! videoconvert ! textoverlay name=overlay1 ! videoconvert ! glimagesink sync=false
+  demux.src_0 ! queue ! videoconvert ! textoverlay name=overlay0 ! videoconvert ! autovideosink sync=false
+  demux.src_1 ! queue ! videoconvert ! textoverlay name=overlay1 ! videoconvert ! autovideosink sync=false
   tee name=t ! queue ! overlay0.text_sink
   t.src_1 ! queue ! overlay1.text_sink
 """
